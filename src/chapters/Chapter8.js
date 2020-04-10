84106ecd12513d0c4bce98764830eca46e636c6a
guru <guru.softwaremaster@mail.ru>
2020-04-10 16:20:44 +0300

    Chpater8 updated

--- src/chapters/Chapter8.js
+++ src/chapters/Chapter8.js
@@ -1,114 +1,73 @@
 import React from 'react'
 
-const infoReducer = (state, action) => {
-  return {
-    ...state,
-    [action.name]: action.value,
-  }
-}
-
-
+// custom compoent
 const Info = () => {
+  const [name, setName] = React.useState('')
+  const [nickname, setNickname] = React.useState('')
+
+  // useEffect
+  // mount + state change
   React.useEffect(() => {
-    console.log('Rendering Completed')
+    console.log('Render')
+    
+    // cleanup function
+    return () => {
+      console.log("%c Cleanup function", "background-color: black; color : white;")
+    }
   })
 
-  const [state, dispatch] = React.useReducer(infoReducer, {
-    name: '',
-    nickname: '',
-  })
-  return (
-    <div>
-        <h2> Using useReducer </h2>
-      <div>
-        <input
-          value={state.name}
-          name="name"
-          onChange={(e) => dispatch(e.target)}
-        />
-        <input
-          value={state.nickname}
-          name="nickname"
-          onChange={(e) => dispatch(e.target)}
-        />
-      </div>
-      <div>
-        <p>
-          <b> Name </b> {state.name}
-        </p>
-        <p>
-          <b> Nickname </b> {state.nickname}
-        </p>
-      </div>
-    </div>
-  )
-}
+  // only when componet mount, consider second parameter
+  React.useEffect(() => {
+    console.log('%c Only Mount Hook', 'color: green')
 
-function reducer(state, action) {
-  switch (action.type) {
-    case 'INC':
-      return { value: state.value + 1 }
-    case 'DEC':
-      return { value: state.value - 1 }
-    default:
-      return state
-  }
-}
-
-const Counter = () => {
-  const [state, dispatch] = React.useReducer(reducer, { value: 0 })
-  return (
-    <div>
-      <p>
-        Current Value is <b> {state.value} </b>
-      </p>
-      <button onClick={() => dispatch({ type: 'INC' })}> INC </button>
-      <button onClick={() => dispatch({ type: 'DEC' })}> DEC </button>
-    </div>
-  )
-}
-
-const Average = () => {
-  const [list, setList] = React.useState([])
-  const [number, setNumber] = React.useState('')
-
-  const onChange = React.useCallback((e) => {
-    setNumber(parseInt(e.target.value))
+    // cleanup function when only dismounted
+    return () => {
+      console.log("%c Only Dismount Cleanup function", "background-color: black; color : white;")
+    }
   }, [])
-  
-  const onInsert = React.useCallback((e) => {
-    const nextList = [...list, number]
-    setList(nextList)
-  }, [number, list])
 
-  const getAverage = (list) => {
-      console.log("Average called")
-      if(list.length === 0) return 0
-      const sum = list.reduce((total, item) => (total + item))
-      return sum/list.length
-  }
 
-  const avg = React.useMemo(() => getAverage(list), [list])
+  //  when only name state change
+  React.useEffect(() => {
+    console.log('%c Name State Changed', 'color: blue')
+  }, [name])
+
+  // when only nickname state change
+  React.useEffect(() => {
+    console.log('%c NickName State Changed', 'color: brown')
+  }, [nickname])
+
   return (
     <div>
-        <h2> Using Memo </h2>
-      {list.map((value, index) => (
-        <li key={index}> {value} </li>
-      ))}
-      <p> Average : {avg}</p>
-      <input onChange={onChange} />
-      <button onClick={onInsert}> onInsert</button>
+      <h2> React custom component</h2>
+      <input
+        name="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        name="nickname"
+        value={nickname}
+        onChange={(e) => setNickname(e.target.value)}
+      />
+      <div> 
+        <span style={{color: "blue" }}>name : {name}  </span>
+        <br />
+        <span style={{color: "brown" }}>nickname : {nickname}  </span>
+      </div>
     </div>
   )
 }
-
 const Chapter8 = () => {
+  // let's define visible
+  const [visible, setVisible] = React.useState(true)
   return (
     <div>
-      <h1> React - HOOK </h1>
-      <Info />
-      <Counter />
-      <Average />
+      <h1> React Hook Example - useEffect </h1>
+      <button onClick={() => { setVisible(!visible)} }>
+        {visible? "Hide" : "Show"} 
+      </button>
+      {visible && <Info/>}
     </div>
   )
 }
