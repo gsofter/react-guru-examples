fab02ca29baab0ae321b5ff086c8589953007b38
guru <guru.softwaremaster@mail.ru>
2020-04-16 03:26:07 +0300

    update: chapter10 styles

--- src/chapters/Chapter10.js
+++ src/chapters/Chapter10.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import './styles/chapter10/TodoTemplate.scss'
 import { MdAdd } from 'react-icons/md'
+import {
+    MdCheckBoxOutlineBlank,
+    MdCheckBox,
+    MdRemoveCircleOutline
+} from 'react-icons/md'
 const TodoTemplate = ({ children }) => {
   return (
     <div className="TodoTemplate">
@@ -20,12 +25,37 @@ const TodoInsert = () => {
     </form>
   )
 }
+
+const TodoListItem = () => {
+    return(
+        <div className = "TodoListItem">
+            <div className="checkbox">
+                <MdCheckBoxOutlineBlank />
+            </div>
+            <div className="remove">
+                <MdRemoveCircleOutline />
+            </div>
+        </div>
+    )
+}
+
+const TodoList = () => {
+    return (
+        <div className="TodoList">
+            <TodoListItem />
+            <TodoListItem />
+            <TodoListItem />
+        </div>
+    )
+}
+
 const Chapter10 = (props) => {
   return (
     <div>
       <h1> Chapter10 </h1>
       <TodoTemplate>
         <TodoInsert />
+        <TodoList />
       </TodoTemplate>
     </div>
   )
