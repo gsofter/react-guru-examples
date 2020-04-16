98db953aab812d59d3c9bf20f4919f4ef272ee1e
guru <guru.softwaremaster@mail.ru>
2020-04-16 04:47:41 +0300

    update: chapter10 - todoitem add, remove function added!

--- src/chapters/Chapter10.js
+++ src/chapters/Chapter10.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState, useRef, useCallback } from 'react'
 import './styles/chapter10/TodoTemplate.scss'
 import { MdAdd } from 'react-icons/md'
 import {
-    MdCheckBoxOutlineBlank,
-    MdCheckBox,
-    MdRemoveCircleOutline
+  MdCheckBoxOutlineBlank,
+  MdCheckBox,
+  MdRemoveCircleOutline,
 } from 'react-icons/md'
+
 const TodoTemplate = ({ children }) => {
   return (
     <div className="TodoTemplate">
@@ -15,10 +16,28 @@ const TodoTemplate = ({ children }) => {
   )
 }
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [text, setText] = useState('')
+  const onChange = (e) => {
+    setText(e.target.value)
+  }
+
+  const onSubmit = useCallback(
+    (e) => {
+    console.log('asdf')
+      onInsert(text)
+      setText('')
+      e.preventDefault()
+    },
+    [text, onInsert],
+  )
   return (
-    <form className="TodoInsert">
-      <input placeholder="Input things to do" />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="Input things to do"
+        value={text}
+        onChange={(e) => onChange(e)}
+      />
       <button type="submit">
         <MdAdd />
       </button>
@@ -26,36 +45,83 @@ const TodoInsert = () => {
   )
 }
 
-const TodoListItem = () => {
-    return(
-        <div className = "TodoListItem">
-            <div className="checkbox">
-                <MdCheckBoxOutlineBlank />
-            </div>
-            <div className="remove">
-                <MdRemoveCircleOutline />
-            </div>
-        </div>
-    )
+const TodoListItem = ({ todo, onRemove }) => {
+  const { text, checked } = todo
+  const onClickRemove = useCallback(
+      e => {
+          onRemove()
+      }
+  )
+  return (
+    <div className="TodoListItem">
+      <div className={`checkbox ${checked ? 'checked' : ''}`}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
+      </div>
+      <div className="remove" onClick={onClickRemove}>
+        <MdRemoveCircleOutline />
+      </div>
+    </div>
+  )
 }
 
-const TodoList = () => {
-    return (
-        <div className="TodoList">
-            <TodoListItem />
-            <TodoListItem />
-            <TodoListItem />
-        </div>
-    )
+const TodoList = ({ todos, onRemove}) => {
+  return (
+    <div className="TodoList">
+      {todos.map((todo) => {
+        return <TodoListItem todo={todo} key={todo.id} onRemove={() => onRemove(todo.id)} />
+      })}
+    </div>
+  )
 }
 
 const Chapter10 = (props) => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'Understanding basic concept of React',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'Component  Styling',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: 'Todoapp make',
+      checked: false,
+    },
+  ])
+
+  const nextId = useRef(4)
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        chceked: false,
+      }
+      setTodos(todos.concat(todo))
+      nextId.current += 1
+    },
+    [todos],
+  )
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id))
+    },
+    [todos],
+  )
+
   return (
     <div>
       <h1> Chapter10 </h1>
       <TodoTemplate>
-        <TodoInsert />
-        <TodoList />
+        <TodoInsert onInsert={onInsert} />
+        <TodoList todos={todos} onRemove={onRemove}/>
       </TodoTemplate>
     </div>
   )
