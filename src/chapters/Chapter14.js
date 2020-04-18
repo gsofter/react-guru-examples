50a1c90807c3318b4140e5f4ace73d07949a792e
guru <guru.softwaremaster@mail.ru>
2020-04-18 04:44:04 +0300

    Update: chpater14 - NewsList add

--- src/chapters/Chapter14.js
+++ src/chapters/Chapter14.js
@@ -1,20 +1,125 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-const Chapter14 = () => {
-  const [data, setData] = useState(null)
-  const onClick = () => {
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then((res) => {
-      setData(res.data)
-    })
-  }
+import styled from 'styled-components'
+const API_KEY = "5ed644a60cfc45bcaedc0fe478c008fd"
 
+const NewsItemBlock = styled.div`
+    display: flex;
+    .thumbnail {
+        margin-right: 1rem;
+        img {
+            display: block;
+            width: 160px;
+            height: 100px;
+            object-fit: cover;
+        }
+    }
+
+    .contents {
+        h2{
+            margin: 0;
+            a{
+                color: black;
+            }
+        }
+
+        p {
+            margin: 0;
+            line-height: 1.5;
+            margin-top:0.5rem;
+            white-space: normal;
+        }
+    }
+
+    & + & {
+        margin-top: 3rem;
+    }
+`
+
+const NewsItem = ({article}) => {
+    const {title, description, url, urlToImage} = article;
+    return (
+        <NewsItemBlock>
+            {urlToImage && (
+                <div className="thumbnail">
+                    <a href={url} target="_blank" rel="noopener noreferer">
+                        <img src={urlToImage} alt="thumbnail" />
+                    </a>
+                </div>
+            )}
+            <div className="contents">
+                <h2> 
+                    <a href={url} target="_blank" rel="reopener noreferer">
+                        {title}
+                    </a>
+                </h2>
+                <p> {description}</p>
+            </div>
+        </NewsItemBlock>
+    )
+}
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const sampleArticle = {
+    title: 'title',
+    description: 'content',
+    url: 'https://google.com',
+    urlToImage: 'https://via.placeholder.com/160',
+  };
+
+const NewsList = () => {
+    const [articles, setArticles] = useState(null)
+    const [loading, setLoading] = useState(false)
+  
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true)
+            try {
+                const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+                setArticles(response.data.articles)
+            } catch(e) {
+                console.log(e)
+            }
+            setLoading(false)
+        }
+        fetchData();
+    }, [])
+  
+    if(loading) {
+        return <NewsListBlock> Loading .... </NewsListBlock>
+    }
+  
+    if(!articles) {
+        return null
+    }
+
+    return (
+        <NewsListBlock>
+            {articles.map(article => (
+                <NewsItem key={article.url} article={article} />
+            ))}
+        </NewsListBlock>
+    )
+    
+};
+
+const Chapter14 = () => {
+  
   return (
     <div>
-      <h1> Chapter 14 - External API Sync</h1>
-      <button onClick={onClick}> Load JSON </button>
-      <div>
-        {data && <textarea value={JSON.stringify(data, null, 2)} readOnly/> }
-      </div>
+      <NewsList />
     </div>
   )
 }
